f3faaa6fa8d09a86cf29c76b1dad5834307bcd0c
AleAlle62 <basketallegrini@gmail.com>
2023-04-13 11:23:09 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,12 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public float Price { get; set; }
+        public string Img { get; set; } = string.Empty;
+
+    }
+}
